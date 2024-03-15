2c906605fc16207a0f5e75c60bc49636f6588de4
CreatorLZ <isaacanyim3@gmail.com>
2024-03-15 20:21:00 +0100

    peace

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -8,7 +8,7 @@ export default function About() {
     <main>
       <div className="w-full flex flex-col">
         <h2 className="text-4xl font-black">About Me.</h2>
-        <p className=" leading-loose sm:leading-relaxed text-left text-xs md:text-base ">
+        <p className=" leading-loose sm:leading-relaxed text-left text-sm md:text-base ">
           Hi, nice to meet you{'!'}.I {''}m Isaac C Anyim, Frontend developer
           with <strong> 2 years{'+'}</strong> of experience working with various
           frontend technologies to bulid assesible, engaging and user-centric
--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -296,6 +296,25 @@ export default function Projects() {
             <Link href={projects[selectedProject].github}>
               {projects[selectedProject].github}
             </Link>
+            <div className=" w-full h-fit p-5 bg-black text-white text-sm sm:text-base flex items-center justify-center mt-3">
+              <Link href={projects[selectedProject].link}>
+                <p className="text-white">Open project</p>
+              </Link>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
+                />
+              </svg>
+            </div>
           </div>
         )}
       </div>
