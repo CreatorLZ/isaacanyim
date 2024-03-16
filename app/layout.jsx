f21b6737ea94b14058e12b49a4c95899b569467d
CreatorLZ <isaacanyim3@gmail.com>
2024-03-16 03:24:45 +0100

    peace

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <Navbar />
         {children}
         <Footer />
