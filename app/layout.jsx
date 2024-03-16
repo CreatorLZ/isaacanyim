33cac787f1e32c379eea8304e6c6ba1d37247c05
CreatorLZ <isaacanyim3@gmail.com>
2024-03-16 02:58:34 +0100

    peace

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,14 +8,15 @@ import Footer from './components/Footer'
 const inter = Anybody({ subsets: ['latin'], variable: '--font-inter' })
 
 export const metadata = {
-  title: 'Isaac Chimaroke Anyim',
-  description: 'isaac Chimaroke Anyim|Portfolio',
+  title: 'Isaac Chimaroke Anyim|Portfolio',
+  description: 'isaac Chimaroke Anyim|Frontend developer',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <link rel="icon" href="/public/earth.png" sizes="any" />
         <Navbar />
         {children}
         <Footer />
