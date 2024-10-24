574a047682e4f5f49af9a8ddd9bb5dc23c0ff9f2
Isaac Chimaroke <isaacanyim3@gmail.com>
2024-10-24 13:04:33 +0100

    new shit

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -37,46 +37,46 @@ export default function Navbar() {
       <div className="gap-6 items-center px-6 md:px-3 hidden md:flex w-screen md:w-fit justify-between   ">
         <div className="flex gap-5 items-end justify-end ">
           <NavLink className="flex md:flex" href="/about">
-            <span className="relative z-10 block px-1 md:px-3 py-3 overflow-hidden font-medium leading-tight text-black transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+            <span className="relative z-10 block px-1 md:px-3 py-3 overflow-hidden font-medium leading-tight text-gray-700 transition-colors duration-300 ease-out border-2 border-gray-700 rounded-lg group-hover:text-white">
               <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
               <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-primary group-hover:-rotate-180 ease"></span>
               <span className="relative text-gray-700"> About</span>
             </span>
             <span
-              className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+              className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-700 rounded-lg group-hover:mb-0 group-hover:mr-0"
               data-rounded="rounded-lg"
             ></span>
           </NavLink>
           <NavLink className=" md:flex" href="/#skills">
-            <span className="relative z-10 block px-1 md:px-3 py-3 overflow-hidden font-medium leading-tight text-black transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+            <span className="relative z-10 block px-1 md:px-3 py-3 overflow-hidden font-medium leading-tight text-gray-700 transition-colors duration-300 ease-out border-2 border-gray-700 rounded-lg group-hover:text-white">
               <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
               <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-primary group-hover:-rotate-180 ease"></span>
               <span className="relative text-gray-700"> Skills</span>
             </span>
             <span
-              className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+              className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-700 rounded-lg group-hover:mb-0 group-hover:mr-0"
               data-rounded="rounded-lg"
             ></span>
           </NavLink>
           <NavLink className=" md:flex" href="/#projects">
-            <span className="relative z-10 block  px-1 md:px-3 py-3 overflow-hidden font-medium leading-tight text-black transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+            <span className="relative z-10 block  px-1 md:px-3 py-3 overflow-hidden font-medium leading-tight text-gray-700 transition-colors duration-300 ease-out border-2 border-gray-700 rounded-lg group-hover:text-white">
               <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
               <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-primary group-hover:-rotate-180 ease"></span>
               <span className="relative text-gray-700"> Projects</span>
             </span>
             <span
-              className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+              className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-700 rounded-lg group-hover:mb-0 group-hover:mr-0"
               data-rounded="rounded-lg"
             ></span>
           </NavLink>
           <NavLink className=" md:flex" href="/#contact">
-            <span className="relative z-10 block  px-1 md:px-3 py-3 overflow-hidden font-medium leading-tight text-black transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+            <span className="relative z-10 block  px-1 md:px-3 py-3 overflow-hidden font-medium leading-tight text-gray-700 transition-colors duration-300 ease-out border-2 border-gray-700 rounded-lg group-hover:text-white">
               <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
               <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-primary group-hover:-rotate-180 ease"></span>
               <span className="relative text-gray-700"> Contact</span>
             </span>
             <span
-              className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+              className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-700 rounded-lg group-hover:mb-0 group-hover:mr-0"
               data-rounded="rounded-lg"
             ></span>
           </NavLink>
