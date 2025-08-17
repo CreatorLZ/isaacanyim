e6679b2031288ebe7b9d8d75a62f033440fd984b
Isaac Chimaroke <isaacanyim3@gmail.com>
2025-08-17 12:51:05 +0100

    about updates

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -76,7 +76,7 @@ export default function About() {
           open-source, diving headfirst into coding projects, shit posting into
           the void on x, and building interactive, elegant websites.
           <br /> <br /> In my free time, I explore advancements in artificial
-          intelligence and machine learning, or getting lost in a good book.
+          intelligence and machine learning, or get lost in a good book.
           <br /> <br /> I am a firm believer in the power of technology to
           transform lives and communities, and I am committed to using my skills
           to make a positive impact in the world.
