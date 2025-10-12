5c9ac2caae1d46a8939fae82341dcff7f2f7171e
Isaac Chimaroke <isaacanyim3@gmail.com>
2025-10-12 19:16:50 +0100

    feat:cleanup

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -159,7 +159,7 @@ export default function Navbar() {
                 href="/#contact"
                 className="ml-4 bg-[#7856ff] hover:bg-[#6645e0] text-white rounded-full px-6 py-3 hidden lg:flex items-center text-sm uppercase font-bold tracking-wider transition-colors group"
               >
-                HIRE ME
+                SAY HI
                 <ArrowRight className="ml-2 w-3 h-3 -rotate-45 transition-transform group-hover:rotate-0" />
               </Link>
             </motion.div>
