ec5a52f1dbb2efb9749f90417a712bcb95be95d1
CreatorLZ <isaacanyim3@gmail.com>
2026-07-29 17:41:44 +0100

    fix footer typo

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
   return (
     <footer className="flex flex-col text-center pt-20 w-full items-center justify-center z-50 pb-24 lg:pb-0">
       {/* <small className="text-text-muted">Copyright © {new Date().getFullYear()}. Isaac C. Anyim. All rights reserved.</small> */}
-      <small className="text-text-muted">Built Isaac C Anyim</small>
+      <small className="text-text-muted">Built by Isaac C Anyim</small>
       {/* Desktop Icons */}
       <div className="hidden sm:flex gap-10 items-center pt-1 md:pt-6 lg:pt-10 px-2.5 md:px-10 lg:px-36 pb-4 justify-between sm:justify-normal backdrop-blur-sm md:backdrop-blur-none">
         <Link
